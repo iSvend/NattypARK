0ac06b34e15bb3d2ae12e5266c3e63bc4d1cd5a6
Svend Knutsen <sk@svendknutsen.com>
2016-01-10 21:29:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nattyp ark
+
+A simplified version of the ngBoilerplate angular project template.
+
+***
+
+## Quick Start
---
+++ src/app/app.js
@@ -0,0 +1,26 @@
+(function (app) {
+
+	app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+		$urlRouterProvider.otherwise('/');
+		$locationProvider.html5Mode(true);
+	});
+
+    app.run(function () { });
+    app.controller('AppController', function ($scope) {
+
+    });
+    app.controller('NavController', function ($scope, $location) {
+        $scope.isCollapsed = true;
+        $scope.$on('$routeChangeSuccess', function () {
+            $scope.isCollapsed = true;
+        });
+    });
+
+} (angular.module("NattypARK", [
+    'NattypARK.home',
+    'NattypARK.about',
+    'templates-app',
+    'templates-common',
+    'ui.router.state',
+    'ui.router',
+])));
