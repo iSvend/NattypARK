5a7c52068e8c2c9ee293646da6bf51f9c92c0ff7
Svend Knutsen <sk@svendknutsen.com>
2016-01-13 21:25:26 -0700

    Fixed height issues on version slide

--- src/common/version.js
+++
@@ -1,7 +0,0 @@
-angular.module('services')
-
-.service('VersionService', function($http, AppService) {
-    this.getVersion = function() {
-        return $http.get('https://api.ark.bar/v1/version');
-    };
-});
