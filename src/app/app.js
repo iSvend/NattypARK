a7f15fcb76b4697354156807e29ae784497d9439
Svend Knutsen <sk@svendknutsen.com>
2016-01-20 20:18:58 -0700

    fixed missing bottom section

--- src/app/app.js
+++ src/app/app.js
@@ -21,7 +21,7 @@
             //error
         });
 
-        $scope.someList = ['Map - The Island', 'PVPVE', '5x Gather(Harvest)', '5x Faster Taming', '5x Experience'];
+        $scope.someList = ['Map - The Island', 'PVPVE', '3x Gather(Harvest)', '5x Faster Taming', '4x Experience'];
 
         init();
 
